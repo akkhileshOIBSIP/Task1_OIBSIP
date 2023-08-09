4bb0cbd41b01b576d1ddc76cfa564bc8986e3fed
Akkhilesh RP <akkhileshrp@gmail.com>
2023-08-09 21:45:01 +0530

    Changes : Project Completed

--- client/src/components/Screen.jsx
+++ client/src/components/Screen.jsx
@@ -4,7 +4,7 @@ import { Textfit } from 'react-textfit';
 const Screen = () => {
     const { calc } = useContext(CalcContext);
   return (
-    <Textfit className='screen'>084554</Textfit>
+    <Textfit className='screen' max={70} mode='single'>{calc.num ? calc.num : calc.res}</Textfit>
   );
 };
 
